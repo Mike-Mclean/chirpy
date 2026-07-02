784fdc50bc92ca51b3eaa4bb09473e9e8b7aae5a
Mike-Mclean <mmcle009@gmail.com>
2026-07-02 11:27:58 -0400

    Implement middleware for metrics tracking and enhance metrics display, started the implementing JSON responses to requests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 type apiConfig struct {
@@ -10,35 +11,71 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileserverHits.Add(1)
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (cfg *apiConfig) printMetrics(w http.responseWriter, req *http.Request) http.Handler {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) printMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	message := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	message := fmt.Sprintf(`
+	<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`, cfg.fileserverHits.Load())
 	w.Write([]byte(message))
 }
 
-func main() {
-	serve_mutex := http.NewServeMux()
-	serve_mutex.Handle("/app/",
-		middlewareMetricsInc(
-			http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-		)
-	)
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
-	serve_mutex.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+func respondWithError(w http.ResponseWriter, code int, msg string){
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		w.Write([]byte("OK\n"))
 	})
 
-	serve_mutex.HandleFunc("/metrics", printMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, req *http.Request){
+		type chirp struct {
+			Content string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := chirp{}
+		err := decoder.Decode(&params)
+		if err != nil{
+			respondWithError(w, 500, "Something went wrong")
+			return
+		}
+
+		respondWithJSON()
+	})
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.printMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
 
 	server := http.Server{
 	Addr: ":8080",
-	Handler: serve_mutex,
+	Handler: mux,
 	}
 
 	server.ListenAndServe()
